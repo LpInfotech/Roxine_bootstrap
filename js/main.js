6a38f79ae5d6edc42d9294b016935c9f994fd5aa
mukesh <mukeshsalaria01@gmail.com>
2021-12-27 10:14:14 +0530

    changes in navbar

--- js/main.js
+++ js/main.js
@@ -5,13 +5,13 @@ $(window).on('scroll', function () {
     if (scroll >= 50) {
       $(".navbar").addClass("bg-white");
       $(".navbar .nav-link, .navbar .fa-icon").addClass("text-primary").removeClass("text-white");
-      $(".navbar .light-img").addClass("d-none").removeClass("d-block");
-      $(".navbar .dark-img").addClass("d-block").removeClass("d-none");
+      $(".navbar .light-img").addClass("d-none").removeClass("d-lg-block");
+      $(".navbar .dark-img").addClass("d-block").removeClass("d-lg-none");
     }else{
       $(".navbar").removeClass("bg-white");
       $(".navbar .nav-link, .navbar .fa-icon").removeClass("text-primary").addClass("text-white");
-      $(".navbar .light-img").addClass("d-block").removeClass("d-none");
-      $(".navbar .dark-img").addClass("d-none").removeClass("d-block");
+      $(".navbar .light-img").addClass("d-lg-block");
+      $(".navbar .dark-img").addClass("d-lg-none");
     }
   }
   });
